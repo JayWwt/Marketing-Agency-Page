02a59073fe3fef4b548b60ca4cb39f1389c72a88
Jay Patel <jay1.worldweb@gmail.com>
2025-07-22 10:49:06 +0530

    about us section in desktop

---
+++ Js/script.js
@@ -0,0 +1,42 @@
+
+//For Toggle Button
+const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("show");
+    });
+});
+
+
+
+
+
+//For Counter1
+let upto = 0;
+let counts1 = setInterval(updated1, 10);
+
+function updated1() {
+    let count = document.getElementById("counter1");
+    count.innerHTML = ++upto + " " + "+";
+    if (upto === 320) {
+        clearInterval(counts1);
+    }
+}
+
+
+
+
+
+//For Counter2
+let Upto = 0;
+let counts2 = setInterval(updated2, 10);
+
+function updated2() {
+    let count = document.getElementById('counter2');
+    count.innerHTML = ++Upto + " " + "+";
+    if (Upto === 230) {
+        clearInterval(counts2);
+    }
+}
